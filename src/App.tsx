361868b5fd6fb209d904c6df91b5d3e9d0f26b80
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2024-11-05 20:52:39 +0600

    set up routes at top level componen

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { AppMenu, Navbar, Toast } from 'components';
+import { getAppPathName } from 'lib/common';
+import { COMPONENT_ROUTES, DEFAULT_ROUTE } from 'lib/constatns';
+import { ReactNode } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import EditEngagement from 'routes/EditEngagement';
+
+function AppContainer({ children }: { children: ReactNode }) {
+  return (
+    <div className="min-h-screen w-full bg-base-200">
+      <Navbar />
+      <div aria-expanded="false" className="drawer lg:drawer-open">
+        <div className="drawer-content">
+          <div className="pt-24">{children}</div>
+        </div>
+      </div>
+
+      <Toast />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <AppContainer>
+      <Routes>
+        {Object.keys(COMPONENT_ROUTES).map((path) => {
+          const Component = COMPONENT_ROUTES[path];
+          return (
+            <Route
+              key={path}
+              path={getAppPathName(path)}
+              element={
+                <div className="grid grid-cols-1 gap-0 lg:grid-cols-9">
+                  <AppMenu />
+                  <Component />
+                </div>
+              }
+            />
+          );
+        })}
+
+        <Route
+          path={`${DEFAULT_ROUTE}/:id/edit`}
+          element={<EditEngagement />}
+        />
+
+        {/* Redirect to default route */}
+        {['/', '*'].map((path) => (
+          <Route
+            key={path}
+            path={path}
+            element={<Navigate replace to={DEFAULT_ROUTE} />}
+          />
+        ))}
+      </Routes>
+    </AppContainer>
   );
 }
 
