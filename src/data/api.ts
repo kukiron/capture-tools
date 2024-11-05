55fc279976807f408f1fa2d0ac5c4a856f1b1e34
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2024-11-05 20:50:28 +0600

    add mock data & api call

---
+++ src/data/api.ts
@@ -0,0 +1,54 @@
+import { delay } from 'lib/common';
+
+import POST_ENGAGEMENT_DATA from 'data/json/post-engagement.json';
+import { PostEngagement } from 'store/types';
+
+enum AppDataType {
+  POST_ENGAGEMENT = 'post-engagement',
+}
+
+type MockFetchResponse<T> = {
+  json: () => T;
+};
+
+type BaseResponse = { success: boolean; message: string };
+type ApiResponse<T> = BaseResponse & {
+  result?: T;
+};
+
+const RawAppData = {
+  'post-engagement': POST_ENGAGEMENT_DATA,
+} as const;
+
+// A common mock fetch utility to fetch app data
+const fetchAppData = <T>(type: AppDataType): Promise<MockFetchResponse<T>> =>
+  new Promise(async (resolve) => {
+    await delay(Math.random() * 1000);
+    resolve({
+      json: () => RawAppData[type] as T,
+    });
+  });
+
+/* ----- API Calls ----- */
+
+export const fetchPostEngagementData = async (): Promise<
+  ApiResponse<PostEngagement[]>
+> => {
+  try {
+    const response = await fetchAppData<PostEngagement[]>(
+      AppDataType.POST_ENGAGEMENT
+    );
+    const result = response.json();
+    return {
+      success: true,
+      message: 'Post engagements fetched successfully.',
+      result,
+    };
+  } catch (error) {
+    console.log('Error fetching post engagements:', error);
+    return {
+      success: false,
+      message: 'Failed to fetch pot engagements.',
+    };
+  }
+};
