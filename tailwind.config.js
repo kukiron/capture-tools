8dabe5852c3f96118e0ca5625eb641ba56fd5497
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2024-11-06 06:58:14 +0600

    add dynamic class name rules in tailwing config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 
 const TOAST_TYPES = ['success', 'error', 'info', 'warning'];
 const POST_REACTIONS = ['like', 'love', 'haha', 'wow', 'sad', 'angry'];
+const TOOLTIP_POSITIONS = ['top', 'right', 'bottom', 'left'];
 
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
@@ -16,7 +17,11 @@ module.exports = {
   },
   safelist: [
     ...TOAST_TYPES.map((type) => `alert-${type || 'info'}`),
-    ...POST_REACTIONS.map((type) => `reaction-${type}`),
-    ...POST_REACTIONS.map((type) => `like-btn-${type}`),
+    ...POST_REACTIONS.map(
+      (reaction) => `reaction-${reaction} like-btn-${reaction}`
+    ),
+    ...TOOLTIP_POSITIONS.map(
+      (position) => `tooltip-${position} lg:tooltip-${position}`
+    ),
   ],
 };
