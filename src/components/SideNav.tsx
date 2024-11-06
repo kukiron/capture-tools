de77c3f0d65867bfe89eeaceb4fecdefb94db130
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2024-11-06 11:57:29 +0600

    add navigation to side-nav for capture tools option

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,14 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+
 import { useAppDispatch } from 'hooks';
-import { DEFAULT_TOAST_MESSAGE, SIDENAV_ITEMS } from 'lib/constatns';
+import {
+  DEFAULT_ROUTE,
+  DEFAULT_TOAST_MESSAGE,
+  SIDENAV_ITEMS,
+} from 'lib/constatns';
 import { showToast } from 'store/reducers/toast';
 
 import Icon from './Icon';
 import Tooltip from './Tooltip';
 
 function SideNav() {
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
-  const handleClick = () => {
+  const handleClick = (name: string) => {
+    if (name === 'Capture tools') {
+      navigate(DEFAULT_ROUTE);
+      return;
+    }
     dispatch(showToast({ message: DEFAULT_TOAST_MESSAGE }));
   };
 
@@ -16,10 +27,7 @@ function SideNav() {
     <div className="min-h-screen">
       <ul className="menu min-h-full border-r border-r-base-300 bg-base-100 pt-20">
         {SIDENAV_ITEMS.map(({ name, icon }) => (
-          <li
-            key={name}
-            onClick={() => name !== 'Capture tools' && handleClick()}
-          >
+          <li key={name} onClick={() => handleClick(name)}>
             <span
               className={`${name === 'Capture tools' ? 'active' : ''} cursor-pointer p-0`}
             >
