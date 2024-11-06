3e0ecf6f02dba4d96d6f4d47f3b60ad6bc43ba21
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2024-11-06 07:00:00 +0600

    add more app icons

--- src/components/Icon/icons-by-name.ts
+++ src/components/Icon/icons-by-name.ts
@@ -1,18 +1,23 @@
 import { BiHome } from 'react-icons/bi';
 
 import {
+  BsChatText,
   BsFiletypeJson,
+  BsGear,
   BsInfoCircle,
   BsPlugin,
   BsShieldExclamation,
+  BsSpeedometer2,
   BsWallet2,
 } from 'react-icons/bs';
 
-import { FiLifeBuoy, FiLink, FiUser } from 'react-icons/fi';
+import { FiLifeBuoy, FiLink, FiUser, FiUsers } from 'react-icons/fi';
 
 import { GoCheckCircle } from 'react-icons/go';
 
-import { IoMdBook, IoMdLogOut } from 'react-icons/io';
+import { ImMagicWand, ImMagnet } from 'react-icons/im';
+
+import { IoIosMegaphone, IoMdBook, IoMdLogOut } from 'react-icons/io';
 
 import {
   MdArrowDropDown,
@@ -27,7 +32,7 @@ import {
 
 import { PiWarningCircleBold } from 'react-icons/pi';
 
-import { RiCloseCircleLine, RiMoonLine } from 'react-icons/ri';
+import { RiCloseCircleLine, RiMoonLine, RiStackLine } from 'react-icons/ri';
 
 import {
   TbBrandMessenger,
@@ -36,21 +41,29 @@ import {
   TbSearch,
 } from 'react-icons/tb';
 
+import { VscGraphLine } from 'react-icons/vsc';
+
 // alphabetical order
 export const ICONS_BY_NAME = {
   account: FiUser,
   'add-post': MdOutlinePostAdd,
   'arrow-right': MdKeyboardArrowRight,
   'arrow-left': MdKeyboardArrowLeft,
+  automation: ImMagicWand,
   book: IoMdBook,
+  broadcast: IoIosMegaphone,
   plugin: BsPlugin,
+  chat: BsChatText,
   'check-circle': GoCheckCircle,
   'close-circle': RiCloseCircleLine,
   close: MdOutlineClose,
   community: TbBuildingCommunity,
+  dashboard: BsSpeedometer2,
   dropdown: MdArrowDropDown,
+  engagement: ImMagnet,
   'fast-forward': MdFastForward,
   'fast-rewind': MdFastRewind,
+  gear: BsGear,
   home: BiHome,
   'info-circle': BsInfoCircle,
   json: BsFiletypeJson,
@@ -62,7 +75,10 @@ export const ICONS_BY_NAME = {
   'qr-code': TbQrcode,
   search: TbSearch,
   shield: BsShieldExclamation,
+  stacks: RiStackLine,
   sun: MdOutlineWbSunny,
+  users: FiUsers,
+  graph: VscGraphLine,
   wallet: BsWallet2,
   'warning-circle': PiWarningCircleBold,
 } as const;
